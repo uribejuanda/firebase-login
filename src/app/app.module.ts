2c37803db704e43da89a672a0fafe604764209b5
Juanda Uribe <j.uribe@globant.com>
2017-11-21 00:21:24 -0500

    Login basic implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutingModule } from './routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -19,7 +22,9 @@ import { LoginComponent } from './login/login.component';
         BrowserModule,
         BrowserAnimationsModule,
         MaterialModule,
-        RoutingModule
+        RoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule // imports firebase/auth, only needed for auth features
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+    login() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
 
   ngOnInit() {
   }
