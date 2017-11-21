ce5e609d89be5ddf2ceb0c67a79e35ac5c76b716
Juanda Uribe <j.uribe@globant.com>
2017-11-21 00:02:28 -0500

    Routing config added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MaterialModule
+        MaterialModule,
+        RoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    }, {
+        path: 'home', component: HomeComponent
+    }, {
+        path: 'login', component: LoginComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: []
+})
+export class RoutingModule { }
