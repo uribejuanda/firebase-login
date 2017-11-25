11efd4812b8372d1e22f2e590c43cf53282b4821
Juanda Uribe <j.uribe@globant.com>
2017-11-24 19:22:36 -0500

    login buttons added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,23 +4,32 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth) { }
+    constructor(public afAuth: AngularFireAuth) {
+    }
 
-    login() {
+    loginWithGoogle() {
         this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+
+    loginWithFacebook() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    }
+
+    loginWithEmail() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.PhoneAuthProvider());
     }
 
     logout() {
         this.afAuth.auth.signOut();
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
